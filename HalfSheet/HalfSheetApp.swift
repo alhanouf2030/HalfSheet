0a5cada113f56889f83a153787b188c6de814504
alhanouf2030 <alhanouf228@gmail.com>
2022-04-04 11:14:00 +0300

    Initial Commit

---
+++ HalfSheet/HalfSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfSheetApp.swift
+//  HalfSheet
+//
+//  Created by alhanouf on 03/09/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct HalfSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
